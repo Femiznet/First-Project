7c8863f542802a8815d500b9934a66aa3ebfdb7e
Femiznet <ajuwonfemi121@gmail.com>
2025-10-05 16:17:53 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("This is the test-branch version of hello.py")
+import requests
+
+response=requests.get("https://dog.ceo/api/breeds/list/all")
+
+dict=response.json()
+
+
+gool=[]
+
+with open("dogs.txt", "w") as o:
+
+    f=dict["message"]
+
+    for key,value in dict.items():
+
+        if key == "message":
+
+            for xkeys,xvalue in value.items():
+
+                if f[xkeys] != []:
+
+                    gool.append(f"{xkeys}={f[xkeys]}")
+
+    o.write("\n".join(gool))
+
+
